3b4e16d007f7e102653497c2d3bcd5e353d78333
Om Prakash Thakur <omthakurcloudengineer@gmail.com>
2025-08-08 18:39:54 +0000

    still here no any image is showings

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
